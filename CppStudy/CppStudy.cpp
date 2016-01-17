bdeac5b5fcc1f230d891b524a5e000253ba071e7
Eric Liu <wakengine@gmail.com>
2016-01-17 19:00:32 +0800

    Test

---
+++ CppStudy/CppStudy.cpp
@@ -0,0 +1,23 @@
+// CppStudy.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <vector>
+//#include <agri>
+
+
+using std::vector;
+
+int main()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+
+	for_each()
+	printf("Hello World!\n");
+
+    return 0;
+}
+
